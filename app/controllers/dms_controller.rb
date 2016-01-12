47e6ac9d8a6953780841bc3a4e5eeae8b4848cdc
Marimo <arashi.scandal74@gmail.com>
2016-01-12 10:04:50 +0900

    yoga add

--- app/controllers/dms_controller.rb
+++ app/controllers/dms_controller.rb
@@ -2,6 +2,7 @@ class DmsController < ApplicationController
 
   before_action :move_to_index, except: :index
 
+
   def index
     @comments = Dm.where(opponent: params[:id]).where(contributor: current_useraccount.id)
     @comment = Dm.new
@@ -12,11 +13,13 @@ class DmsController < ApplicationController
   def create
     Dm.create(comment: comment_params[:comment], contributor: current_useraccount.id, opponent: params[:opponent])
     @comments = Dm.where(opponent: params[:opponent]).where(contributor: current_useraccount.id)
+    # binding.pry
   end
 
   def destroy
     Dm.find(params[:id]).destroy
-    @comments = Dm.where(opponent: params[:opponent]).where(contributor: current_useraccount.id)
+    @comments = Dm.where(opponent: params[:id]).where(contributor: current_useraccount.id)
+    binding.pry
   end
 
   private
