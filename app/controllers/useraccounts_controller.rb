1606c561bf69d25fef4ce9c1caa3aa91799218b1
Marimo <arashi.scandal74@gmail.com>
2016-01-15 11:50:26 +0900

    マイページへのルーティング

---
+++ app/controllers/useraccounts_controller.rb
@@ -0,0 +1,5 @@
+class UseraccountsController < ApplicationController
+  def show
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :useraccounts
+  resources :useraccounts, only: [:show]
   root 'chats#index'
   get 'dms/:id' => 'dms#index'
   resources :chats, only: [:create, :destroy]
