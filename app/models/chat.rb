1a150b4700c84a389923cfb0d5cde657806b9831
t-kusakabe <ttmac0704@gmail.com>
2016-02-12 19:37:46 +0900

    コメントのバリデーションを追加

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,6 @@
 class Chat < ActiveRecord::Base
   belongs_to :useraccount, foreign_key: 'contributor', class_name: 'Useraccount'
   belongs_to :group, foreign_key: 'id', class_name: 'Group'
+
+  validates :comment, presence: true
 end
